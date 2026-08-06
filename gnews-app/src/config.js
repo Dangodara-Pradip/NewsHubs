fadb442c9d8566e1989dd9d1c8a8aa9c11291954
Pradip Dangodara <pradip@example.com>
2026-08-06 17:16:09 +0530

    Use env var for GNews API key

--- gnews-app/src/config.js
+++ gnews-app/src/config.js
@@ -1,2 +1,2 @@
-// Put your GNews API key here (get one free at https://gnews.io)
+// Put your GNews API key in an environment variable named REACT_APP_GNEWS_API_KEY
 export const API_KEY = "1aaf07e41d86531a04207ee90ca616f3";
